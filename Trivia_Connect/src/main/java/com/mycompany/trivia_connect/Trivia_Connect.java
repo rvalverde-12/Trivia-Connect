804e4ceb9a056ca36735f2e228d74306f5b2bb73
Snaider <>
2025-11-13 22:26:05 -0600

    HOLA

---
+++ Trivia_Connect/src/main/java/com/mycompany/trivia_connect/Trivia_Connect.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trivia_connect;
+
+/**
+ *
+ * @author serra
+ */
+public class Trivia_Connect {
+
+    public static void main(String[] args) {
+        System.out.println("Hola equipo!");
+    }
+}
